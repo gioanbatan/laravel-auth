ea17dbcdc12d8a2ea0f6664fcdd17288e36ccf9a
gioanbatan <giovannibassani@hotmail.com>
2023-01-10 15:57:43 +0100

    Add: provvisory layout and faker population of db

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,6 +9,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return ('prova-prova');
+        return view('admin.dashboard');
     }
 }
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('it_IT');
+
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+
+            $project->title = $faker->sentence(2);
+            $project->description = $faker->text(400);
+            $project->slug  = Str::slug($project->title, '-');
+
+            $project->save();
+        }
+    }
+}
